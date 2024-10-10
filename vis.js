a2dc1e862387991fc030ebc4a2aab544212ace98
Maitrey0152 <msha0152@student.monash.edu>
2024-10-10 22:44:13 +1100

    Uploading files for visualization

---
+++ vis.js
@@ -0,0 +1,20 @@
+var vs1 = "map.json";
+vegaEmbed("#map",vs1).then(function(result){
+
+}).catch(console.error);
+
+var vs4 = "area.json";
+vegaEmbed("#area",vs4).then(function(result){
+
+}).catch(console.error);
+
+var vs2 = "bar.json";
+vegaEmbed("#bar",vs2).then(function(result){
+
+}).catch(console.error);
+
+var vs3 = "stacked.json";
+vegaEmbed("#stacked",vs3).then(function(result){
+
+}).catch(console.error);
+
